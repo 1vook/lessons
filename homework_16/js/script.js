64c42eac9d1a5a03d7e8940b1e71d8e1de146c90
Oleksii Fedchenko <70106980+1vook@users.noreply.github.com>
2025-06-03 14:18:29 +0300

    Add files via upload

---
+++ homework_16/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', documentActions)
+function documentActions(e) {
+   const targetElement = e.target
+   if (targetElement.closest('.icon-menu')) {
+      document.body.classList.toggle('menu-open')
+   }
+}
